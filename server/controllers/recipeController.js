258307becc5363935b9b002695e41428df28cf79
Mironshoh1603 <mironshohasadov1603.gmail.com>
2022-07-09 15:43:27 +0500

    fixed

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -5,36 +5,45 @@ const Recipe = require("../models/Recipe");
 /**
  * GET /
  * Homepage
- */
+require('../models/database');
+const Category = require('../models/Category');
+const Recipe = require('../models/Recipe');
+
+/**
+ * GET /
+ * Homepage 
+*/
 exports.homepage = async (req, res) => {
   try {
     const limitNumber = 5;
     const categories = await Category.find({}).limit(limitNumber);
-    // const latest = await Recipe.find({}).sort({_id: -1}).limit(limitNumber);
-    // const thai = await Recipe.find({ 'category': 'Thai' }).limit(limitNumber);
-    // const american = await Recipe.find({ 'category': 'American' }).limit(limitNumber);
-    // const chinese = await Recipe.find({ 'category': 'Chinese' }).limit(limitNumber);
+    const latest = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
+    const thai = await Recipe.find({ category: "Thai" }).limit(limitNumber);
+    const american = await Recipe.find({ category: "American" }).limit(
+      limitNumber
+    );
+    const chinese = await Recipe.find({ category: "Chinese" }).limit(
+      limitNumber
+    );
 
-    // const food = { latest, thai, american, chinese };
+    const food = { latest, thai, american, chinese };
 
-    res.render("index", { title: "Cooking Blog - Home ", categories });
+    res.render("index", { title: "Cooking Blog - Home", categories, food });
   } catch (error) {
     res.satus(500).send({ message: error.message || "Error Occured" });
   }
 };
-exports.expoleCategories = async (req, res) => {
+
+/**
+ * GET /categories
+ * Categories
+ */
+exports.exploreCategories = async (req, res) => {
   try {
     const limitNumber = 20;
     const categories = await Category.find({}).limit(limitNumber);
-    // const latest = await Recipe.find({}).sort({_id: -1}).limit(limitNumber);
-    // const thai = await Recipe.find({ 'category': 'Thai' }).limit(limitNumber);
-    // const american = await Recipe.find({ 'category': 'American' }).limit(limitNumber);
-    // const chinese = await Recipe.find({ 'category': 'Chinese' }).limit(limitNumber);
-
-    // const food = { latest, thai, american, chinese };
-
     res.render("categories", {
-      title: "Cooking Blog - Categories ",
+      title: "Cooking Blog - Categoreis",
       categories,
     });
   } catch (error) {
